41d478cbacb067d3f46809f81f9de0d6337a3aa7
Manish Hansda <62429936+manish-hnsd@users.noreply.github.com>
2026-05-15 11:12:52 +0530

    Changes

--- src/streamApi/Strings.java
+++ src/streamApi/Strings.java
@@ -11,33 +11,36 @@ public class Strings {
         characterCount();
         charCountString();
         reverseAString();
+        findVowels();
     }
     static void lower2Upper(){ //Lower to Upper Case
         List<String>  list = Arrays.asList("abcd");
         List<String> result = list.stream().map(String::toUpperCase).collect(Collectors.toList());
-        System.out.println(result);
+//        System.out.println(result);
     }
     static void sort(){ //Sort in descending order
         List<String> list = Arrays.asList("abcd");
-        List<String> result = list.stream().map(str -> new StringBuilder(str).reverse().toString()).collect(Collectors.toList());
-        System.out.println(result);
+//        List<String> result = list.stream().map(str -> new StringBuilder(str).reverse().toString()).collect(Collectors.toList());
+        List<String> result2 = list.stream().map(str -> str.chars().mapToObj(c -> String.valueOf((char) c))
+                .sorted(Comparator.reverseOrder()).collect(Collectors.joining())).toList();
+//        System.out.println(result2);
     }
     static void characterCount(){ // Occurrence of Char
-        List<String>  list = Arrays.asList("a","a","b","b","c","d","e");
-        Map<String, Long> charCount = list.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
-        System.out.println(charCount);
+        List<String>  list = Arrays.asList("aabbcde");
+        Map<Character, Long> charCount = list.stream().flatMap(str -> str.chars().mapToObj(c -> (char) c))
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+//        System.out.println(charCount);
     }
     static void charCountString(){// Occurrence of Char
         String str = "Manish"; //Using Stream
         Map<String,Long> result = Arrays.stream(str.split("")).collect(Collectors.groupingBy(Function.identity(),
                 Collectors.counting()));
-        System.out.println(result);
+//        System.out.println(result);
     }
     static void reverseAString(){ // Reverse a String
         String str = "Manish";
-        String reversed = str.chars().mapToObj(c -> String.valueOf((char) c)).collect(Collectors.collectingAndThen(Collectors.toList(),
-                list -> {java.util.Collections.reverse(list); return String.join("", list);}));
-        System.out.println(reversed);
+        String reverse = str.chars().mapToObj(c -> String.valueOf((char) c)).reduce("", (a,b) -> b+a);
+//        System.out.println(reverse);
     }
     static void firstNonRepeatedCharacter(){ //First Non-Repeating Char
         String str = "aabbcde";
@@ -46,5 +49,11 @@ public class Strings {
                         Collectors.counting())).entrySet().stream().filter(entry -> entry.getValue() == 1) //first non-repeated
                 .map(Map.Entry::getKey).findFirst().orElse(null);
         System.out.println(result);
+    }
+    static void findVowels(){
+        String str = "Manish"; // Find vowels from a string
+        String vowels = str.chars().mapToObj(c -> String.valueOf((char) c))
+                .filter(ch -> "aeiouAEIOU".indexOf(ch) != -1).collect(Collectors.joining());
+        System.out.println(vowels);
     }
 }
