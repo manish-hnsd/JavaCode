1995df490212ae8614d4836b240d1231e83611ef
Manish Hansda <62429936+manish-hnsd@users.noreply.github.com>
2026-06-04 00:00:28 +0530

    Changes

--- src/dsa/TwoPointers/MoveZeroes.java
+++ src/dsa/TwoPointers/MoveZeroes.java
@@ -1,4 +1,28 @@
 package dsa.TwoPointers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MoveZeroes {
+    public static void main(String[] args) {
+        int [] nums = {0,0,1,2,0,5}; //Output: [1,2,5,0,0,0]
+        System.out.println(Arrays.toString(moveZero(nums)));
+    }
+    public static int[] moveZero(int[] nums) {
+        List<Integer> temp = new ArrayList<>();
+        for(int num : nums){
+            if(num != 0){
+                temp.add(num);
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (i < temp.size()){
+                nums[i] = temp.get(i);
+            }else {
+                nums[i] = 0;
+            }
+        }
+        return nums;
+    }
 }
